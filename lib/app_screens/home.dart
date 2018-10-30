22d1cfe317b150c26692c32fb0ff6bd5dada4c15
shanmukh.kask <shanmukh.kask@gmail.com>
2018-10-30 17:09:58 +0530

    fonts

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -9,9 +9,21 @@ class Home extends StatelessWidget{
       child:  Container(
         alignment: Alignment.center,
         color: Colors.tealAccent,
-        width: 300.0,
-        height: 100.0,
-        child: Text("Flutter",textDirection: TextDirection.ltr,),
+
+      //  width: 300.0,
+    //    height: 100.0,
+       //   margin: EdgeInsets.only(left:20.0,top: 40.0),
+        child: Text(
+            "Flutter",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 75.0,
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+              color: Colors.green
+          ),
+        ),
       )//Container,
     );
 
